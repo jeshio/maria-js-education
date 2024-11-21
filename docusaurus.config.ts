80cf2b99dff641acf9630aca6570cd09cf69e2e7
jeshio <jeshio@yandex.ru>
2024-11-21 17:25:21 +0500

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://jeshio.github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -137,6 +137,10 @@ const config: Config = {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
     },
+
+    projectName: 'jeshio.github.io',
+    organizationName: 'jeshio',
+    trailingSlash: false,
   } satisfies Preset.ThemeConfig,
 };
 
